7d5e2426355dc9d3b0774ed66b823abf20714830
mo1977 <60240693+mo1977@users.noreply.github.com>
2020-01-29 18:59:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route("/health.json")  
+def health():
+    return jsonify({"status": "UP"}), 200
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0',port=7777,debug=True)
+
+
+
 
